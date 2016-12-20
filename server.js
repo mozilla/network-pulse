7234f85fb3b10b26dab0970e659bd67d5a9f7482
Mavis Ou <mmmavisss@gmail.com>
2016-12-19 18:29:31 -0800

    now uses express server instead of pushstate-server

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var server = require(`pushstate-server`);
+const express = require(`express`);
+const path = require(`path`);
 
-server.start({
-  port: process.env.PORT || 3000,
-  directory: `./dist`,
-  file: `index.html`
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, `dist`)));
+
+app.get(`*`, (req, res) => {
+  res.sendFile(path.resolve(__dirname, `index.html`));
+});
+
+app.listen(PORT, () => {
+  console.log(`Network Pulse listening on port ${PORT}...`);
 });
