767bb563aecd2e7277e811e06b9caa91797417c5
Mavis Ou <mmmavisss@gmail.com>
2016-12-19 20:17:02 -0800

    config changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname, `dist`)));
 
 app.get(`*`, (req, res) => {
-  res.sendFile(path.resolve(__dirname, `index.html`));
+  res.sendFile(path.resolve(__dirname, `dist`, `index.html`));
 });
 
 app.listen(PORT, () => {
