d16616dfae0e3e31c1c18e7c0113e0fe748af5a1
Aayush Sanghavi <aayushsanghavi26@gmail.com>
2017-01-06 13:20:58 +0530

    added read more function and removed unnecessary meta data and visit function

--- js/project-list.js
+++ js/project-list.js
@@ -1,4 +1,4 @@
-/* Project Card */
+/* Project List */
 
 var ProjectList = {
   template: _.template($("script#project-list-template").html()),
@@ -16,43 +16,32 @@ var ProjectList = {
       title: projectData.Title ? projectData.Title : "",
       creators: projectData.Creators ? projectData.Creators : "",
       timestamp: month + " " + timestamp.getDate() + ", " + timestamp.getFullYear(),
-      description: projectData.Description ? projectData.Description : "",
-      thumbnail: projectData['Thumbnail URL'] ? projectData['Thumbnail URL'] : "",
       interest: projectData.Interest ? projectData.Interest : "",
       networkConnection: projectData['Network connection'] ? projectData['Network connection'] : "",
       origin: projectData.Origin ? projectData.Origin : "",
-      tags: projectData.Tags,
-      issues: issues,
-      belongsToOnlinePrivacyAndSecurityIssue: issuesAsArray.indexOf("Online Privacy & Security") > -1,
-      belongsToOpenInnovationIssue: issuesAsArray.indexOf("Open Innovation") > -1,
-      belongsToDecentralizationIssue: issuesAsArray.indexOf("Decentralization") > -1,
-      belongsToWebLiteracyIssue: issuesAsArray.indexOf("Web Literacy") > -1,
-      belongsToDigitalInclusionIssue: issuesAsArray.indexOf("Digital Inclusion") > -1,
-      program: projectData.Program,
-      type: projectData.Type,
-      link: projectData.URL ? projectData.URL : "",
-      getInvolvedText: projectData['Get involved'] ? projectData['Get involved'] : "",
-      getInvolvedLink: projectData['Get involved URL'] ? projectData['Get involved URL'] : "",
       detailViewLink: utility.getProjectDirectLink(id),
-      thumbnailView: false
     };
     return this.template(dataForTemplate);
   },
-  visitBtnClickHandler: function(event) {
+  readMoreLinkClickHandler: function(event) {
+    event.preventDefault();
+    var $projectList = $(this).parents(".project-list");
+    var projectId = $projectCard.attr("id");
     ga('send', {
       hitType: 'event',
-      eventCategory: "Visit Button",
+      eventCategory: "Read More Link",
       eventAction: 'click',
-      eventLabel: $(this).parents(".project-list").data("title"),
-      transport: "beacon"
+      eventLabel: $projectList.data("title")
     });
+    utility.updateUrlWithoutReload("id",projectId);
+    ViewsManager.showSingleProjectView(projectId);
   },
   render: function(projectData) {
     var id = projectData.id;
     var isStarred = FavouritesManager.isProjectFavourited(id);
     var featured = projectData.Featured;
     var html = ProjectList.buildHTML(projectData);
-    var $card = $(html);
-    $("#project-container .projects").append($card);
+    var $list = $(html);
+    $("#project-container .projects").append($list);
   }
 };
