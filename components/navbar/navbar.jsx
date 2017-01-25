2dda47491925dde44c57beffa58de899a94493ef
Mavis Ou <mmmavisss@gmail.com>
2017-01-24 19:17:15 -0800

    Fixes #326 - renamed non user facing 'favorites' instances in code to 'bookmarks'

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -15,6 +15,12 @@ export default React.createClass({
     }
   },
   render() {
+    // We have renamed all non user facing "favorites" related variables and text (e.g., favs, faved, etc) to "bookmarks".
+    // This is because we want client side code to match what Pulse API uses (i.e., bookmarks)
+    // For user facing bits like UI labels and URL path we want them to stay as "favorites".
+    // That's why a link like <SelectorLink to="/favs" className="text-nav-link bookmarks">Favs</SelectorLink> is seen here.
+    // For more info see: https://github.com/mozilla/network-pulse/issues/326
+
     return (
       <div className="navbar">
         <div className="container">
@@ -26,7 +32,7 @@ export default React.createClass({
               <li><SelectorLink to="/featured" className="text-nav-link">Featured</SelectorLink></li>
               <li><SelectorLink to="/latest" className="text-nav-link">Latest</SelectorLink></li>
               <li><SelectorLink to="/issues" className="text-nav-link">Issues</SelectorLink></li>
-              <li><SelectorLink to="/favs" className="text-nav-link favs">Favs</SelectorLink></li>
+              <li><SelectorLink to="/favs" className="text-nav-link bookmarks">Favs</SelectorLink></li>
               <li><SelectorLink to="/search" className="btn-search"><i className="fa fa-search"/><span className="sr-only">Search</span></SelectorLink></li>
               <li><SelectorLink to="/add" className="btn-add" onClick={this.handleAddPageLinkClick} ref={(link) => { this.btnAdd = link; }}><img src="/assets/svg/icon-plus.svg" /></SelectorLink></li>
             </ul>
