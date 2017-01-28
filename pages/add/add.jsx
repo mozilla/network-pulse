e7c724e1601d093ad4b747be8df6ab2ba03eba18
Mavis Ou <mmmavisss@gmail.com>
2017-01-27 16:34:41 -0800

    don't use inline function in render(); removed the incomplete form UI code from this PR

--- pages/add/add.jsx
+++ pages/add/add.jsx
@@ -39,123 +39,46 @@ export default React.createClass({
     event.preventDefault();
     this.setState({numCreatorFields: this.state.numCreatorFields+1});
   },
-  render() {
-    let creatorFields = [];
-
-    for (var i=0; i<this.state.numCreatorFields; i++) {
-      creatorFields.push( <CreatorInputField key={i} />);
+  getContentForLoggedInUser() {
+    return( <div>
+              <p>(TODO:FIXME: form for logged in user to be built)</p>
+              <p>Hi<span className="user-full-name"> {user.username}</span></p>
+              <p className="log-out-prompt">Not you? <button className="btn btn-link" onClick={this.handleLogOutBtnClick}>Sign out</button>.</p>
+            </div>);
+  },
+  getFailurePrompt() {
+    return ( <HintMessage imgSrc={`/assets/svg/icon-user.svg`}
+                          header={`Sign in failed`}
+                          internalLink={`/featured`}
+                          linkText={`Explore featured`}>
+              <p>Only Mozilla staff can login now as we test this new platform. Check back soon!</p>
+            </HintMessage>);
+  },
+  getAnonymousContent() {
+    let redirectUrl = `${env.ORIGIN}${this.props.router.getCurrentLocation().pathname}`;
+    return (<HintMessage imgSrc={`/assets/svg/icon-user.svg`}
+                         header={`Please sign in to add a post`}
+                         externalLink={user.getLoginURL(redirectUrl)}
+                         linkText={`Sign in`}
+                         onClick={this.handleSignInBtnClick}>
+              <p>Please note, only Mozilla staff can login now as we test this new platform.</p>
+            </HintMessage>);
+  },
+  getContent() {
+    if (user.loggedin) {
+      return this.getContentForLoggedInUser();
     }
 
-    let redirectUrl = `${env.ORIGIN}${this.props.router.getCurrentLocation().pathname}`;
-    let anonymousContent = (<HintMessage imgSrc={`/assets/svg/icon-user.svg`}
-                                         header={`Please sign in to add a post`}
-                                         externalLink={user.getLoginURL(redirectUrl)}
-                                         linkText={`Sign in`}
-                                         onClick={this.handleSignInBtnClick}>
-                            <p>Please note, only Mozilla staff can login now as we test this new platform.</p>
-                          </HintMessage>);
+    if (user.failedLogin) {
+      return this.getFailurePrompt();
+    }
 
-
-    let failurePrompt= ( <HintMessage imgSrc={`/assets/svg/icon-user.svg`}
-                                      header={`Sign in failed`}
-                                      internalLink={`/featured`}
-                                      linkText={`Explore featured`}>
-                          <p>Only Mozilla staff can login now as we test this new platform. Check back soon!</p>
-                        </HintMessage>);
-
-    let contentForLoggedInUser = (
-      <div>
-        <h1>Share with the Network</h1>
-        <p>Do you have something to share? If it might be useful to someone in our network, share it here! Pulse includes links to products and software tools, research reports and findings, think pieces, white papers, interviews, and curricula. If it might be useful, share it … at any stage or fidelity.</p>
-        <form action="" method="post">
-          <h2>Basic Info</h2>
-          <div className="posted-by">
-            <p>Posted by:&nbsp;<span className="user-full-name">{user.username}</span></p>
-            <p className="log-out-prompt">Not you? <button className="btn btn-link" onClick={this.handleLogOutBtnClick}>Sign out</button>.</p>
-          </div>
-          <fieldset hidden>
-            <label className="form-control-label">
-              Posted by:&nbsp;
-              <input name="" type="text" className="form-control" value="" readOnly />
-            </label>
-          </fieldset>
-          <label className="form-control-label">
-            Title of the project<em className="required">(required)</em>
-            <input name="title" type="text" placeholder="Title" className="form-control" />
-          </label>
-          <label className="form-control-label">
-            URL<em className="required">(required)</em>
-            <input name="" type="url" placeholder="https://example.com" className="form-control" />
-          </label>
-          <label className="form-control-label">
-            Describe what you are sharing. Keep it simple and use plain language.
-            <textarea name="description" placeholder="Description" className="form-control" />
-          </label>
-          <h2>Optional Details</h2>
-          <fieldset className="creators">
-            <legend>Who are the creators? This could be staff, contributors, partners…</legend>
-            { creatorFields }
-            <button onClick={this.handleCreatorClick} className="btn btn-link">&#43; Add another creator</button>
-          </fieldset>
-          <label className="form-control-label">
-            Keywords to help with search by program, event, campaign, subject …
-            <input name="tags" type="text" placeholder="#mozfest  #code  #tool" className="form-control" />
-          </label>
-          <fieldset>
-            <legend>Check any Key Internet Issues that relate to your project.</legend>
-            <label className="form-control-label">
-              <input name="issues" type="checkbox" className="form-control" value="Online Privacy & Security" />Online Privacy & Security
-            </label>
-            <label className="form-control-label">
-              <input name="issues" type="checkbox" className="form-control" value="Open Innovation" />Open Innovation
-            </label>
-            <label className="form-control-label">
-              <input name="issues" type="checkbox" className="form-control" value="Decentralization" />Decentralization
-            </label>
-            <label className="form-control-label">
-              <input name="issues" type="checkbox" className="form-control" value="Web Literacy" />Web Literacy
-            </label>
-            <label className="form-control-label">
-              <input name="issues" type="checkbox" className="form-control" value="Digital Inclusion" />Digital Inclusion
-            </label>
-          </fieldset>
-
-
-          <label className="form-control-label">
-            Looking for support? Describe how people can do that.
-            <input name="get_involved" type="text" placeholder="Contribute to the code." className="form-control" />
-          </label>
-          <label className="form-control-label">
-            Link for people to get involved.
-            <input name="get_involved_url" type="url" placeholder="https://example.com" className="form-control" />
-          </label>
-          <fieldset>
-            <label className="form-control-label">
-              Upload a thumbnail image
-              <input name="thumbnail_url" type="url" placeholder="https://example.com" className="form-control" />
-            </label>
-            <p className="form-text"><em>1200px by 630px recommended image dimension</em></p>
-          </fieldset>
-          <button type="submit" className="btn btn-filled">Submit project</button>
-        </form>
-      </div>
-    );
-
-    let getContent = function() {
-      if (user.loggedin) {
-        return contentForLoggedInUser;
-      }
-
-      if (user.failedLogin) {
-        return failurePrompt;
-      }
-
-      return anonymousContent;
-    };
-
+    return this.getAnonymousContent();
+  },
+  render() {
     return (
       <div className="add-page">
-        { getContent() }
+        { this.getContent() }
       </div>
     );
   }
--- pages/bookmarks.jsx
+++ pages/bookmarks.jsx
@@ -31,23 +31,27 @@ export default React.createClass({
     // let's verify what the logged in status is
     user.verify(this.props.router.location, () => this.setState({ user }));
   },
+  getContentForLoggedInUser() {
+    return(<p>Hi {user.username}! <button onClick={this.handleLogOutBtnClick} className="btn-link">Log out</button>.</p>);
+  },
+  getFailurePrompt() {
+    return (<p>Only Mozilla staff can login now as we test this new platform. Meanwhile, your favorited projects can still be remembered on this device.</p>);
+  },
+  getAnonymousContent() {
+    return (<p>Are you Mozilla Staff? <button className="btn btn-link" onClick={this.handleSignInBtnClick}>Sign in</button>.</p>);
+  },
+  getloggedInStatusContent() {
+    if (user.loggedin) {
+      return this.getContentForLoggedInUser();
+    }
+
+    if (user.failedLogin) {
+      return this.getFailurePrompt();
+    }
+
+    return this.getAnonymousContent();
+  },
   render() {
-    let anonymousContent = <p>Are you Mozilla Staff? <button className="btn btn-link" onClick={this.handleSignInBtnClick}>Sign in</button>.</p>;
-    let contentForLoggedInUser = <p>Hi {user.username}! <button onClick={this.handleLogOutBtnClick} className="btn-link">Log out</button>.</p>;
-    let failurePrompt = <p>Only Mozilla staff can login now as we test this new platform. Meanwhile, your favorited projects can still be remembered on this device.</p>;
-
-    let getloggedInStatus = function() {
-      if (user.loggedin) {
-        return contentForLoggedInUser;
-      }
-
-      if (user.failedLogin) {
-        return failurePrompt;
-      }
-
-      return anonymousContent;
-    };
-
     // We have renamed all non user facing "favorites" related variables and text (e.g., favs, faved, etc) to "bookmarks".
     // This is because we want client side code to match what Pulse API uses (i.e., bookmarks)
     // For user facing bits like UI labels and URL path we want them to stay as "favorites".
@@ -56,7 +60,7 @@ export default React.createClass({
 
     return (
       <div>
-        <div className="logged-in-status">{ getloggedInStatus() }</div>
+        <div className="logged-in-status">{ this.getloggedInStatusContent() }</div>
         { this.state.bookmarks && this.state.bookmarks.length > 0 ?
                             <ProjectList params={{ids: this.state.bookmarks.join(`,`)}} />
                             : <HintMessage imgSrc={`/assets/svg/icon-bookmark-selected.svg`}
