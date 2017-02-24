553757a5666c9c2bd2496cd84530aeebcdd57bd3
Mike Kamermans <pomax@nihongoresources.com>
2017-02-24 11:42:23 -0800

    updated webpack config to respect NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,12 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': process.env.NODE_ENV || 'development'
+      }
+    })
+  ]
 };
