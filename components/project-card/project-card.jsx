0d508955ec71e4e7078c467f10e2c4a6f78e553e
Sudip7 <sudipknayak@gmail.com>
2017-04-07 17:47:57 -0700

    removing deprecated thumbnail_url

--- components/project-card/project-card.jsx
+++ components/project-card/project-card.jsx
@@ -50,7 +50,7 @@ export default React.createClass({
     interest: React.PropTypes.string,
     issues: React.PropTypes.arrayOf(React.PropTypes.string),
     thumbnail: React.PropTypes.string,
-    thumbnailUrl: React.PropTypes.string,
+                     
     created: React.PropTypes.string,
     title: React.PropTypes.string.isRequired,
     contentUrl: React.PropTypes.string,
@@ -147,7 +147,7 @@ export default React.createClass({
     return <h2>{title}</h2>;
   },
   renderThumbnail(detailViewLink) {
-    let thumbnailSource = this.props.thumbnail || this.props.thumbnailUrl;
+    let thumbnailSource = this.props.thumbnail;
 
     if (!thumbnailSource) return null;
 
@@ -172,7 +172,7 @@ export default React.createClass({
           </div>;
   },
   renderCreatorInfo() {
-    if (this.props.creators.length === 0) return null;
+    if (this.props.creators.length == 0) return null;
 
     return <small className="creator d-block text-muted">
              By {this.props.creators.join(`, `)}
