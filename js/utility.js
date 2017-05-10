e48f1b45fab8acf05bbe9e8506d8e208794d48ff
Mavis Ou <mmmavis@users.noreply.github.com>
2017-05-10 15:20:14 -0700

    Fixes #523 - making issues and tags visible on detail view

--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,13 @@
 import env from "../config/env.generated.json";
 
+const ISSUE_MAPPING = {
+  "online-privacy-and-security": `Online Privacy & Security`,
+  "open-innovation": `Open Innovation`,
+  "decentralization": `Decentralization`,
+  "web-literacy": `Web Literacy`,
+  "digital-inclusion": `Digital Inclusion`
+};
+
 const Utils = {
   // transform all snake_case keys in an object into camelCase format
   processEntryData(obj) {
@@ -21,6 +29,18 @@ const Utils = {
       return `${env.ORIGIN}${router.getCurrentLocation().pathname}`;
     }
     return console.error(`No router or window available to get the current url!`);
+  },
+
+  getIssueNameFromUriPath(path) {
+    return ISSUE_MAPPING[path] || ``;
+  },
+
+  getUriPathFromIssueName(issueName) {
+    for(let key in ISSUE_MAPPING) {
+      if (ISSUE_MAPPING[key] === issueName) return key;
+    }
+
+    return ``;
   }
 };
 
