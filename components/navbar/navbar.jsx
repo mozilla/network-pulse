72bb1af510d87fd07b837189b4b82e6790609e0d
Sudip7 <sudipknayak@gmail.com>
2017-07-08 09:34:22 +0530

    code improvement

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -23,7 +23,7 @@ class NavBar extends React.Component {
         <div className="container">
           <div className="row">
             <div className="logo col-md-4 mb-3 mb-sm-0">
-              <li className="d-inline-block"><img src="/assets/svg/pulse-wordmark.svg" width="204" height="34" alt="Mozilla Pulse" /></li>
+              <img src="/assets/svg/pulse-wordmark.svg" width="204" height="34" alt="Mozilla Pulse" />
             </div>
             <ul className="nav-link-list col-md-8 mb-0">
               <NavListItem><NavLink to="/featured" className="text-nav-link">Featured</NavLink></NavListItem>
