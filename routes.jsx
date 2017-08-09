e0fb160498db7c47bae24591dea76fcef2889fb8
Mavis Ou <mmmavis@users.noreply.github.com>
2017-08-09 12:38:24 -0700

    converted a few things to ES6

--- routes.jsx
+++ routes.jsx
@@ -38,8 +38,13 @@ const Tag = (router) => {
         </div>;
 };
 
-const App = React.createClass({
-  pageTitle: `Mozilla Network Pulse`,
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.pageTitle = `Mozilla Network Pulse`;
+  }
+
   render() {
     return (
       <div>
@@ -54,7 +59,7 @@ const App = React.createClass({
       </div>
     );
   }
-});
+}
 
 // We have renamed all non user facing "favorites" related variables and text (e.g., favs, faved, etc) to "bookmarks".
 // This is because we want client side code to match what Pulse API uses (i.e., bookmarks)
