062d3daba8711f826ec81e9941325c12b1621f81
Mavis Ou <mmmavis@users.noreply.github.com>
2017-08-15 16:13:32 -0700

    Related to #614 and #628 - use zilla and change footer colour to black (#658)

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ import user from '../../js/app-user';
 class NavListItem extends React.Component {
   render() {
     return (
-      <li className="d-inline-block mb-0">{this.props.children}</li>
+      <li className="d-inline-block mb-0 mr-5">{this.props.children}</li>
     );
   }
 }
@@ -36,7 +36,7 @@ class NavBar extends React.Component {
   renderModeratorLink() {
     if (!user.moderator) return null;
 
-    return <NavListItem><NavLink to="/moderation" className="text-nav-link bookmarks">Moderation</NavLink></NavListItem>;
+    return <NavListItem><NavLink to="/moderation">Moderation</NavLink></NavListItem>;
   }
 
   render() {
@@ -45,6 +45,8 @@ class NavBar extends React.Component {
     // For user facing bits like UI labels and URL path we want them to stay as "favorites".
     // That's why a link like <NavLink to="/favs" className="text-nav-link bookmarks">Favs</NavLink> is seen here.
     // For more info see: https://github.com/mozilla/network-pulse/issues/326
+
+
     return (
       <div className="navbar">
         <div className="container">
@@ -53,10 +55,10 @@ class NavBar extends React.Component {
               <IndexLink to="/" className="d-inline-block"><img src="/assets/svg/pulse-wordmark.svg" width="204" height="34" alt="Mozilla Pulse" /></IndexLink>
             </div>
             <ul className="nav-link-list col-md-8 mb-0">
-              <NavListItem><NavLink to="/featured" className="text-nav-link">Featured</NavLink></NavListItem>
-              <NavListItem><NavLink to="/latest" className="text-nav-link">Latest</NavLink></NavListItem>
-              <NavListItem><NavLink to="/issues" className="text-nav-link">Issues</NavLink></NavListItem>
-              <NavListItem><NavLink to="/favs" className="text-nav-link bookmarks">Favs</NavLink></NavListItem>
+              <NavListItem><NavLink to="/featured">Featured</NavLink></NavListItem>
+              <NavListItem><NavLink to="/latest">Latest</NavLink></NavListItem>
+              <NavListItem><NavLink to="/issues">Issues</NavLink></NavListItem>
+              <NavListItem><NavLink to="/favs" className="bookmarks">Favs</NavLink></NavListItem>
               { this.renderModeratorLink() }
               <NavListItem><NavLink to="/search" className="btn-search"><i className="fa fa-search"/><span className="sr-only">Search</span></NavLink></NavListItem>
               <NavListItem><NavLink to="/add" className="btn-add"><img src="/assets/svg/icon-plus.svg" /></NavLink></NavListItem>
