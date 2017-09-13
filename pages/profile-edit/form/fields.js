80fc04aa9a81db64d32d24c1bd3f2663bc4b7d52
Mavis Ou <mmmavis@users.noreply.github.com>
2017-09-12 19:25:38 -0700

    wip, can now use the form to post profile to backend

--- pages/profile-edit/form/fields.js
+++ pages/profile-edit/form/fields.js
@@ -1,84 +1,117 @@
 import React from 'react';
 import { Link } from 'react-router';
+import validator from '../../../js/form-validator';
 import IssuesField from '../../../components/form-fields/issues.jsx';
-import HelpTypesField from '../../../components/form-fields/help-types.jsx';
+// import HelpTypesField from '../../../components/form-fields/help-types.jsx';
 
-// FIXME:TO: field names are subject to changes. Once we figure out what fields backend is accepting.
+// TODO:FIXME: field names are subject to changes. Once we figure out what fields backend is accepting.
 
 let IssuesLabel = () => {
   return <div>Your <Link to="/issues" target="_blank">Key Internet Issues</Link> interest.</div>;
 };
 
 module.exports = {
-  "city_country": {
+  "custom_name": {
     type: `text`,
-    label: `City, Country`,
-    placeholder: `Vancouver, Canada`,
+    label: `Public name`,
+    placeholder: ``,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.emptyValueValidator()
+    ]
   },
-  "languages": {
-    type: `text`,
-    label: `Languages`,
-    placeholder: `English, Türkçe`,
-    fieldClassname: `form-control`,
-    validator: []
-  },
-  "bio": {
+  // TODO:FIXME: backend currently doesn't have this field
+  // "city_country": {
+  //   type: `text`,
+  //   label: `City, Country`,
+  //   placeholder: `Vancouver, Canada`,
+  //   fieldClassname: `form-control`,
+  //   validator: []
+  // },
+
+  // TODO:FIXME: backend currently doesn't have this field
+  // "languages": {
+  //   type: `text`,
+  //   label: `Languages`,
+  //   placeholder: `English, Türkçe`,
+  //   fieldClassname: `form-control`,
+  //   validator: []
+  // },
+
+  "user_bio": {
     type: `textarea`,
     label: `Bio`,
     placeholder: `About you`,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.maxLengthValidator(140)
+    ]
   },
   "twitter": {
     type: `text`,
     label: `Twitter`,
     placeholder: `https://twitter.com/username`,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.urlValidator(`Twitter`)
+    ],
+    value: `aaa`
   },
   "linkedin": {
     type: `text`,
     label: `LinkedIn`,
     placeholder: `https://linkedin.com/in/username`,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.urlValidator(`LinkedIn`)
+    ]
   },
   "github": {
     type: `text`,
     label: `GitHub`,
     placeholder: `https://github.com/username`,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.urlValidator(`GitHub`)
+    ]
   },
   "website": {
     type: `text`,
     label: `Your website URL`,
     placeholder: `https://example.com`,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.urlValidator()
+    ]
   },
   "thumbnail": {
     type: `image`,
     label: `Profile pic`,
     prompt: `Select image`,
     fieldClassname: `form-control`,
-    validator: []
+    validator: [
+      validator.imageTypeValidator(),
+      validator.imageSizeValidator(),
+      validator.imageFilenameValidator()
+    ]
   },
   "issues": {
     type: IssuesField,
     label: <IssuesLabel />,
     colCount: 1
   },
-  "help_types": {
-    type: HelpTypesField,
-    label: `You can help other people with.`,
-  },
-  "show_fav": {
-    type: `checkboxGroup`,
-    label: `Allow anyone to see a list of posts I have favoritied.`,
-    options: [ `Yes, please show my favs` ],
-    colCount: 1
-  }
+
+  // TODO:FIXME: backend currently doesn't have this field
+  // "help_types": {
+  //   type: HelpTypesField,
+  //   label: `You can help other people with.`,
+  // },
+
+  // TODO:FIXME: backend currently doesn't have this field
+  // "show_fav": {
+  //   type: `checkboxGroup`,
+  //   label: `Allow anyone to see a list of posts I have favoritied.`,
+  //   options: [ `Yes, please show my favs` ],
+  //   colCount: 1
+  // }
 };
--- pages/profile-edit/profile-edit.jsx
+++ pages/profile-edit/profile-edit.jsx
@@ -5,7 +5,8 @@ import { Form } from 'react-formbuilder';
 import HintMessage from '../../components/hint-message/hint-message.jsx';
 import utility from '../../js/utility';
 import user from '../../js/app-user';
-import fields from './form/fields';
+import Service from '../../js/service';
+import formFields from './form/fields';
 
 const PRE_SUBMIT_LABEL = `Submit`;
 const SUBMITTING_LABEL = `Submitting...`;
@@ -13,8 +14,40 @@ const SUBMITTING_LABEL = `Submitting...`;
 export default React.createClass({
   getInitialState() {
     return {
-      user
+      user,
+      currentProfileLoaded: false,
+      fields: {},
+      formValues: {},
+      serverError: false,
+      submitting: false,
+      showFormInvalidNotice: false
     };
+  },
+  componentWillMount() {
+    // get current profile data and load it into form
+    Service.myProfile.get().then(profile => {
+      console.log(`currently stored profile`, profile);
+      let fields = formFields;
+      let values = {};
+
+      Object.keys(formFields).forEach(name => {
+        if (fields[name].type === `image`) {
+          fields[name].defaultImagePath = profile[name];
+          // profile[name] is just the path to the current profile pic
+          // and not the object that contains name of the file and its base64 data
+          // therefore, no need to add it to the values object
+        } else {
+          fields[name].value = profile[name];
+          values[name] = profile[name];
+        }
+      });
+
+      this.setState({
+        fields,
+        formValues: values,
+        currentProfileLoaded: true
+      });
+    });
   },
   componentDidMount() {
     user.addListener(this);
@@ -36,28 +69,79 @@ export default React.createClass({
   },
   handleLogOutBtnClick(event) {
     event.preventDefault();
-    // TODO:FIXME
-    // placeholder event handler
+
     user.logout();
     browserHistory.push({
       pathname: `/featured`
     });
   },
-  handleFormUpdate() {
-    // TODO:FIXME
-    // placeholder event handler
+  handleFormUpdate(event, name, field, value) {
+    let formValues = this.state.formValues;
+
+    formValues[name] = value;
+    this.setState({
+      formValues,
+      // hide notice once user starts typing again
+      // this is a quick fix. for context see https://github.com/mozilla/network-pulse/pull/560
+      showFormInvalidNotice: false
+    });
   },
   handleFormSubmit(event) {
     event.preventDefault();
-    // TODO:FIXME
-    // placeholder event handler
+
+    this.refs.form.validates(formIsValid => {
+      console.log(`this.state.formValues`, this.state.formValues);
+
+      if (!formIsValid) {
+        this.setState({ showFormInvalidNotice: true });
+        return;
+      }
+
+      this.setState({
+        showFormInvalidNotice: false,
+        submitting: true
+      }, () => this.updateProfile(this.state.formValues));
+    });
+  },
+  updateProfile(profile) {
+    // TODO:FIXME:
+    // is_group seems to be a required field?????
+    profile.is_group = false;
+
+    Service.myProfile
+      .put(profile)
+      .then(response => {
+        console.log(`[updated!] response`, response);
+
+        // TODO:FIXME: redirect route to be determined
+        // browserHistory.push({
+        //   pathname: `/profile/id`,
+        // });
+      })
+      .catch(reason => {
+        this.setState({
+          serverError: true
+        });
+        console.error(reason);
+      });
+  },
+  renderForm() {
+    if (!this.state.currentProfileLoaded) return null;
+
+    return <Form ref="form" fields={this.state.fields}
+      inlineErrors={true}
+      onUpdate={(event, name, field, value) => this.handleFormUpdate(event, name, field, value)}
+      className="row" />;
   },
   getContentForLoggedInUser() {
+    let authErrorMessage = this.state.authError ? <span className="error">You seem to be logged out at this moment. Try <a href={user.getLoginURL(utility.getCurrentURL())}>logging in</a> again?</span> : null;
+    let serverErrorMessage = this.state.serverError ? <span className="error">Sorry! We're unable to submit entry to server at this time.</span> : null;
+
     return( <div className="row">
               <div className="col-12">
                 <h1>Your profile</h1>
                 <p>[FIXME] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere a magna a vulputate. Morbi vel odio tincidunt sem pulvinar dapibus ut at odio <span style={{color: `red`}}>link to legal</span>.</p>
-                <p>All fields are optional.</p>
+                <p>All fields are optional. <span style={{color: `red`}}>(is this still true??? "custom_name" seems to be required on backend)</span></p>
                 <div className="mb-4">
                   <div className="mb-3">
                     <span>Email <em>(email won't be displayed or shared)</em></span>
@@ -73,17 +157,17 @@ export default React.createClass({
                     </div>
                   </div>
                 </div>
-                <Form ref="form" fields={fields}
-                                   inlineErrors={true}
-                                   onUpdate={(event) => this.handleFormUpdate(event)}
-                                   className="row" />
+                { this.renderForm() }
                 <div className="submit-section">
                   <button
                     className="btn btn-info mr-3"
                     type="submit"
-                    onClick={this.handleFormSubmit}
+                    onClick={(event) => this.handleFormSubmit(event)}
                     disabled={this.state.submitting ? `disabled` : null}
                   >{ this.state.submitting ? SUBMITTING_LABEL : PRE_SUBMIT_LABEL }</button>
+                  { authErrorMessage }
+                  { serverErrorMessage }
+                  { this.state.showFormInvalidNotice && <span>Something isn't right. Check your info above.</span> }
                 </div>
               </div>
             </div>);
