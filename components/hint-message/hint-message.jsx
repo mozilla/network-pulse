02e215d297ba32c447da79ce071b7ec91102bdb4
Mavis Ou <mmmavis@users.noreply.github.com>
2017-09-27 15:15:49 -0700

    updated 'please log in' message on Edit Profile page

--- components/hint-message/hint-message.jsx
+++ components/hint-message/hint-message.jsx
@@ -12,7 +12,7 @@ class HintMessage extends React.Component {
   render() {
     return (
       <div className="hint-message text-center">
-        <div className="icon mb-3">{ this.props.iconComponent }</div>
+        <h2 className="icon mb-3">{ this.props.iconComponent }</h2>
         <h2>{ this.props.header }</h2>
         { this.props.children }
         { this.renderLink() }
