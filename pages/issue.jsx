d6d660c21b445eb97841ab5709661c11f89f4544
Mavis Ou <mmmavis@users.noreply.github.com>
2017-12-08 11:53:33 -0800

    Better /issues routes handling (#882)
    Related to #852 better /issues routes handling

--- pages/issue.jsx
+++ pages/issue.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { Helmet } from "react-helmet";
 import IssueSelector from '../components/issue-selector/issue-selector.jsx';
 import ProjectLoader from '../components/project-loader/project-loader.jsx';
 import Utility from '../js/utility.js';
 
 export default function (props) {
-  const issueName = Utility.getIssueNameFromUriPath(props.match.params.issue);
+  const issueParam = decodeURIComponent(props.match.params.issue);
+  let issueName = Utility.getIssueNameFromUriPath(issueParam);
 
-  return <div>
-            <Helmet><title>{issueName}</title></Helmet>
-            <IssueSelector />
-            <ProjectLoader issue={issueName} showCounter={true} />
-          </div>;
+  // render page if issueName is one of the 5 hyphenated all lowercase routes we want to serve
+  // e.g., digital-inclusion, open-innovation
+  if (issueName) {
+    return <div>
+              <Helmet><title>{issueName}</title></Helmet>
+              <IssueSelector />
+              <ProjectLoader issue={issueName} showCounter={true} />
+            </div>;
+  }
+
+  issueName = Utility.getUriPathFromIssueName(issueParam);
+
+  // redirect to the corresponding matching route if issueName is a known "issue"
+  // e.g., "Digital Inclusion", "Open Innovation"
+  if (issueName) {
+    return <Redirect to={`/issues/${issueName}`} />;
+  }
+
+  // issueName can't be recognized, redirect to /issues
+  return <Redirect to="/issues" />;
 }
