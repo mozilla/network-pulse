743ac92637a2f66cf3dd05deaf5372c2d1a30a9f
Mavis Ou <mmmavis@users.noreply.github.com>
2017-12-18 15:39:08 -0800

    further cleanup

--- js/build-env-config.js
+++ js/build-env-config.js
@@ -6,17 +6,15 @@ habitat.load(`.env`);
 var environment = habitat.get(`NODE_ENV`);
 
 if (environment !== `PRODUCTION`) {
-  // note that if you have any env var that is in both .env and .default.env,
+  // note that if you have any env var that is in both .env and default.env,
   // the value in .env will always have the highest priority
   // (i.e., it will override the value of the var you have in default.env)
   habitat.load(`config/default.env`);
 }
 
-const PORT = habitat.get(`PORT`);
-const LEARN_MORE_LINK = habitat.get(`LEARN_MORE_LINK`);
 const CONFIG = {
-  PORT,
-  LEARN_MORE_LINK,
+  PORT: habitat.get(`PORT`),
+  LEARN_MORE_LINK: habitat.get(`LEARN_MORE_LINK`),
   PROJECT_BATCH_SIZE: habitat.get(`PROJECT_BATCH_SIZE`),
   PULSE_API: habitat.get(`PULSE_API`)
 };
