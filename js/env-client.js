912739f91fc4d9a874db0a6c6306235a99fb8634
Gavin Lazar Suntop <gavin@gsuntop.com>
2018-01-05 14:33:12 -0800

    handle client env running on server

--- js/env-client.js
+++ js/env-client.js
@@ -1,4 +1,12 @@
-// Scrape env from dom
-let scrapedEnv = JSON.parse(document.getElementById(`environment-variables`).textContent);
+let env = {};
 
-export default scrapedEnv;
+if (typeof document !== `undefined`) {
+  // Scrape env from dom
+  env = JSON.parse(document.getElementById(`environment-variables`).textContent);
+} else {
+  // If this code is run on the server...just use process.env
+  // There appears to be some isomorphic rendering that blows up without this case
+  env = process.env;
+}
+
+export default env;
