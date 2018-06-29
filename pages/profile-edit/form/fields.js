b0a07c03a3850f1010759187850ed52891ae8b39
Mavis Ou <mmmavis@users.noreply.github.com>
2018-06-28 17:53:20 -0700

    Related to #920 - added 'long bio' field to profile edit form

--- pages/profile-edit/form/fields.js
+++ pages/profile-edit/form/fields.js
@@ -7,80 +7,100 @@ let IssuesLabel = () => {
   return <div>Your <Link to="/issues" target="_blank">Key Internet Issues</Link> interest.</div>;
 };
 
-module.exports = {
-  "custom_name": {
-    type: `text`,
-    label: <div>Name <em>(*required field)</em></div>,
-    placeholder: ``,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.emptyValueValidator(),
-      validator.maxLengthValidator(70)
-    ]
-  },
-  "user_bio": {
-    type: `textarea`,
-    label: <div>Bio <em>(do not include any personal information)</em></div>,
-    placeholder: `About you`,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.maxLengthValidator(140)
-    ],
-    charLimit: 140,
-    charLimitText: function (charCount, charLimit) {
-      return charLimit - charCount;
+module.exports = function(showLongBio) {
+  let fields = {
+    "custom_name": {
+      type: `text`,
+      label: <div>Name <em>(*required field)</em></div>,
+      placeholder: ``,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.emptyValueValidator(),
+        validator.maxLengthValidator(70)
+      ]
+    },
+    "user_bio": {
+      type: `textarea`,
+      label: <div>Bio <em>(do not include any personal information)</em></div>,
+      placeholder: `About you`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.maxLengthValidator(140)
+      ],
+      charLimit: 140,
+      charLimitText: function (charCount, charLimit) {
+        return charLimit - charCount;
+      }
+    },
+    "twitter": {
+      type: `text`,
+      label: `Twitter`,
+      placeholder: `https://twitter.com/username`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.urlValidator(`Twitter`)
+      ]
+    },
+    "linkedin": {
+      type: `text`,
+      label: `LinkedIn`,
+      placeholder: `https://linkedin.com/in/username`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.urlValidator(`LinkedIn`)
+      ]
+    },
+    "github": {
+      type: `text`,
+      label: `GitHub`,
+      placeholder: `https://github.com/username`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.urlValidator(`GitHub`)
+      ]
+    },
+    "website": {
+      type: `text`,
+      label: `Your website URL`,
+      placeholder: `https://example.com`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.urlValidator()
+      ]
+    },
+    "thumbnail": {
+      type: `image`,
+      label: <div>Profile pic <em>(only submit photos you have permission to use in this context)</em></div>,
+      prompt: `Select image`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.imageTypeValidator(),
+        validator.imageSizeValidator(),
+        validator.imageFilenameValidator()
+      ]
+    },
+    "issues": {
+      type: IssuesField,
+      label: <IssuesLabel />,
+      colCount: 1
     }
-  },
-  "twitter": {
-    type: `text`,
-    label: `Twitter`,
-    placeholder: `https://twitter.com/username`,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.urlValidator(`Twitter`)
-    ]
-  },
-  "linkedin": {
-    type: `text`,
-    label: `LinkedIn`,
-    placeholder: `https://linkedin.com/in/username`,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.urlValidator(`LinkedIn`)
-    ]
-  },
-  "github": {
-    type: `text`,
-    label: `GitHub`,
-    placeholder: `https://github.com/username`,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.urlValidator(`GitHub`)
-    ]
-  },
-  "website": {
-    type: `text`,
-    label: `Your website URL`,
-    placeholder: `https://example.com`,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.urlValidator()
-    ]
-  },
-  "thumbnail": {
-    type: `image`,
-    label: <div>Profile pic <em>(only submit photos you have permission to use in this context)</em></div>,
-    prompt: `Select image`,
-    fieldClassname: `form-control`,
-    validator: [
-      validator.imageTypeValidator(),
-      validator.imageSizeValidator(),
-      validator.imageFilenameValidator()
-    ]
-  },
-  "issues": {
-    type: IssuesField,
-    label: <IssuesLabel />,
-    colCount: 1
+  };
+
+  if (showLongBio) {
+    fields.user_bio_long = {
+      type: `textarea`,
+      label: <div>About you<div className="text-muted">The following markdown syntax are supported in this field: *bold* _italics_ [link](url)</div></div>,
+      placeholder: `Tell us more about youself, your goals, or where you come from. Do not include any private information!`,
+      fieldClassname: `form-control`,
+      validator: [
+        validator.maxLengthValidator(4096)
+      ],
+      charLimit: 4096,
+      charLimitText: function (charCount, charLimit) {
+        return charLimit - charCount;
+      }
+    };
   }
+
+  return fields;
 };
