83e3cc36eb2a6e433eba86affbc2133163362c8d
Youri Wims <youri@mozillafoundation.org>
2019-05-10 16:29:16 -0400

    final footer style updates

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,79 +1,80 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-class MofoFooter extends React.Component {
+export default class MofoFooter extends React.Component {
   renderSocialLinks() {
-    return this.props.socialLinks.map(socialLink => {
-      return (
+    return (
+      <ul className="d-flex pl-0">
         <li className="mr-2">
           <a
-            className={`${socialLink.iconType} small d-inline-block`}
-            href={socialLink.link}>
-            <span className="sr-only">{socialLink.text}</span>
+            className="twitter small d-inline-block"
+            href="https://twitter.com/mozilla">
+            <span className="sr-only">Twitter</span>
           </a>
         </li>
-      );
-    });
+        <li className="mr-2">
+          <a
+            className="instagram small d-inline-block"
+            href="https://www.instagram.com/mozilla/">
+            <span className="sr-only">Instagram</span>
+          </a>
+        </li>
+        <li className="mr-2">
+          <a
+            className="github small d-inline-block"
+            href="https://www.github.com/mozilla/foundation.mozilla.org/">
+            <span className="sr-only">Github</span>
+          </a>
+        </li>
+      </ul>
+    );
   }
   renderFooterLinks() {
-    return this.props.footerLinks.map(footerLink => {
-      return (
-        <li className="col-auto" key={footerLink.text}>
-          <a
-            className={`footer-link-${footerLink.iconType}`}
-            href={footerLink.link}
-          >
-            {footerLink.text}
-          </a>
+    return (
+      <ul className="link-list list-unstyled mb-0">
+        <li className="col-auto mb-2" key="Donate">
+          <a className="footer-link-donate" href="https://donate.mozilla.org/?utm_source=foundation.mozilla.org&amp;utm_medium=referral&amp;utm_content=footer">Donate</a>
         </li>
-      );
-    });
-  }
-  renderOrgs() {
-    return this.props.orgs.map(org => {
-      // "org-info" is the default classname to be assigned to every organization's wrapper <div>
-      // If there's any custom classname associated with this particular org, append it to the className string.
-      let className = `org-info${org.className ? ` ${org.className}` : ``}`;
-
-      return (
-        <div className={className} key={org.name}>
-          <div className="logo-container">
-            <a className="logo" href={org.link} />
-          </div>
-          {org.description}
-        </div>
-      );
-    });
+        <li className="col-auto mb-2" key="Legal">
+          <a className="footer-link-legal" href="https://www.mozilla.org/about/legal/terms/mozilla/">Legal</a>
+        </li>
+        <li className="col-auto mb-2" key="License">
+          <a className="footer-link-cc-license" href="https://creativecommons.org/licenses/by/4.0">License</a>
+        </li>
+        <li className="col-auto mb-2" key="Participation">
+          <a className="footer-link-undefined" href="https://www.mozilla.org/about/governance/policies/participation/">Participation Guidelines</a>
+        </li>
+        <li className="col-auto mb-2" key="Privacy">
+          <a className="footer-link-undefined" href="https://mozilla.org/en-US/privacy/websites/">Privacy</a>
+        </li>
+      </ul>
+    );
   }
   render() {
     return (
-      <footer className="site-footer dark-theme py-5">
+      <footer className="site-footer dark-theme py-md-5">
         <div className="container">
           <div className="row">
             <div className="col-md-5">
-              <div className="join-us mb-5" />
-              <div className="my-5 mb-md-0">
+              <div className="my-5 my-md-0 d-md-flex align-items-md-end h-100">
                 <a href="https://foundation.mozilla.org" className="logo">
-                  <img src="" alt="" />
+                  <img src="../assets/svg/mozilla-block-white.svg" alt="Mozilla Foundation Home Page" />
                 </a>
               </div>
             </div>
             <div className="col-md-6 offset-md-1 d-flex flex-column justify-content-between">
               <div className="row">
                 <div className="col-md-4">
-                  <h5 class="h5-heading">More about us</h5>
-                  <ul className="d-flex pl-0">{this.renderSocialLinks()}</ul>
+                  <h5 className="h5-heading">More about us</h5>
+                  {this.renderSocialLinks()}
                 </div>
                 <div className="col-md-8">
-                  <ul className="link-list list-unstyled mb-0">
-                    {this.renderFooterLinks()}
-                  </ul>
+                  {this.renderFooterLinks()}
                 </div>
               </div>
               <div className="row">
                 <div className="col-12">
                   <hr className="mt-3 mt-md-5" />
-                  {this.renderOrgs()}
+                  <p className="dark-theme body-small mb-5 mb-md-0">Mozilla is a global non-profit dedicated to putting you in control of your online experience and shaping the future of the web for the public good. Visit us at <a href="https://foundation.mozilla.org">foundation.mozilla.org</a>.</p>
                 </div>
               </div>
             </div>
@@ -83,95 +84,3 @@ class MofoFooter extends React.Component {
     );
   }
 }
-
-MofoFooter.propTypes = {
-  socialLinks: PropTypes.arrayOf(
-    PropTypes.shape({
-      iconType: PropTypes.oneOf([`twitter`, `instagram`, `github`]).isRequired,
-      link: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired,
-  footerLinks: PropTypes.arrayOf(
-    PropTypes.shape({
-      link: PropTypes.string.isRequired,
-      text: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired,
-  orgs: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      link: PropTypes.string.isRequired,
-      description: PropTypes.element.isRequired,
-      className: PropTypes.string
-    }).isRequired
-  ).isRequired
-};
-
-const Footer = () => {
-  const SOCIAL_LINKS = [
-    {
-      iconType: `twitter`,
-      link: `https://twitter.com/mozilla`,
-      text: `Twitter`
-    },
-    {
-      iconType: `instagram`,
-      link: `https://www.instagram.com/mozilla/`,
-      text: `Instagram`
-    },
-    {
-      iconType: `github`,
-      link: `https://www.github.com/mozilla/foundation.mozilla.org/`,
-      text: `Github`
-    }
-  ];
-  const FOOTER_LINKS = [
-    {
-      link: `https://donate.mozilla.org/?utm_source=foundation.mozilla.org&utm_medium=referral&utm_content=footer`,
-      text: `Donate`
-    },
-    {
-      link: `https://mozilla.org/en-US/about/legal/`,
-      text: `Legal`
-    },
-    {
-      link: `https://creativecommons.org/licenses/by/4.0`,
-      text: `License`
-    },
-    {
-      link: `https://www.mozilla.org/about/governance/policies/participation/`,
-      text: `Participation Guidelines`
-    },
-    {
-      link: `https://mozilla.org/en-US/privacy/websites/`,
-      text: `Privacy`
-    }
-  ];
-
-  const ORGS = [
-    {
-      name: `mozilla`,
-      link: `https://mozilla.org`,
-      description: (
-        <p className="dark-theme body-small">
-          Mozilla is a global non-profit dedicated to putting you in control of
-          your online experience and shaping the future of the web for the
-          public good. Visit us at <a href="https://mozilla.org">mozilla.org</a>
-          .
-        </p>
-      ),
-      className: `mozilla`
-    }
-  ];
-
-  return (
-    <MofoFooter
-      footerLinks={FOOTER_LINKS}
-      orgs={ORGS}
-      socialLinks={SOCIAL_LINKS}
-    />
-  );
-};
-
-export default Footer;
