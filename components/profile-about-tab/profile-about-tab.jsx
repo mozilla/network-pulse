670dd018194381754c2195226828eae012386af4
Youri Wims <youri@mozillafoundation.org>
2019-05-14 17:24:34 -0400

    updating responsiveness

--- components/profile-about-tab/profile-about-tab.jsx
+++ components/profile-about-tab/profile-about-tab.jsx
@@ -22,7 +22,7 @@ class ProfileAboutTab extends React.Component {
 
   render() {
     return <div className="profile-about-tab row justify-content-center mb-5 row">
-      <div className="col-8">
+      <div className="col-12 col-sm-10 col-lg-8">
         { this.renderLongBio() }
       </div>
     </div>;
