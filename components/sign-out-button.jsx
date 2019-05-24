4b6cdb2e5c9e3c7dce68734b68d50917b98cb230
Youri Wims <youri@mozillafoundation.org>
2019-05-24 13:03:55 -0400

    made signout link a button and styled it

--- components/sign-out-button.jsx
+++ components/sign-out-button.jsx
@@ -24,7 +24,7 @@ class SignOutButton extends React.Component {
 
   render() {
     return (
-      <a onClick={(event) => this.handleLogOutBtnClick(event)} href="#signout">Sign out</a>
+      <button className="btn-signout" onClick={(event) => this.handleLogOutBtnClick(event)}>Sign out</button>
     );
   }
 }
