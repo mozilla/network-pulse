e4dd5479418df0fec2dd30a226c09812ccb273b1
Lucie Daeye <lucied@mozillafoundation.org>
2019-11-22 14:23:13 +0100

    Add review app bot to network-pulse

---
+++ js/review-app-slack-webhook.js
@@ -0,0 +1,70 @@
+// Post a message on Slack when the Heroku ReviewApp is ready to be used.
+// To run: `node -r dotenv/config js/review-app-slack-webhook.js`
+
+const request = require("request");
+
+// Get the PR's number: we need it to get the PR's title.
+const reviewapp_name = process.env.HEROKU_APP_NAME;
+const re = /\d+/;
+const pr_number = re.exec(reviewapp_name)[0];
+
+const github_token = process.env.GITHUB_TOKEN;
+const org = "mozilla";
+const repo = "network-pulse";
+const slack_webhook = process.env.SLACK_WEBHOOK;
+
+request(
+  `https://api.github.com/repos/${org}/${repo}/pulls/${pr_number}&access_token=${github_token}`,
+  { headers: { "User-Agent": "request" } },
+  function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const pr_title = JSON.parse(body)["title"];
+
+      let color = "";
+      if (JSON.parse(body)["label"]) {
+        for (let label in JSON.parse(body)["label"]) {
+          if (label["name"] === "dependencies") {
+            return (color = "#BA55D3");
+          } else {
+            return (color = "#7CD197");
+          }
+        }
+      } else {
+        return (color = "#7CD197");
+      }
+
+      const slack_payload = {
+        attachments: [
+          {
+            fallback: `New review app deployed: It will be ready in a minute!\n
+                        PR ${pr_number}${pr_title}\n
+                        Login: use your staging credentials\n
+                        URL: https://${reviewapp_name}.herokuapp.com`,
+            pretext: "New review app deployed: It will be ready in a minute!",
+            title: `PR ${pr_number}${pr_title}`,
+            text: "Login: use your staging credentials",
+            color: `${color}`,
+            actions: [
+              {
+                type: "button",
+                text: "View review app",
+                url: `https://${reviewapp_name}.herokuapp.com`
+              },
+              {
+                type: "button",
+                text: "View PR on Github",
+                url: `https://github.com/mozilla/foundation.mozilla.org/pull/${pr_number}`
+              }
+            ]
+          }
+        ]
+      };
+
+      request.post(
+        `${slack_webhook}`,
+        { header: { "Content-Type": "application/json" }, json: slack_payload },
+        function(error, response, body) {}
+      );
+    }
+  }
+);
