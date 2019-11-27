7d7f73b4468a19cd4f0d25d32f7b4dc4f7e2eb87
Mavis Ou <mmmavis@users.noreply.github.com>
2019-11-26 18:56:25 -0800

    remove dev-tool config from production webpack config

--- webpack.server.prod.js
+++ webpack.server.prod.js
@@ -2,6 +2,5 @@ const merge = require("webpack-merge");
 const common = require("./webpack.server.common.js");
 
 module.exports = merge(common, {
-  mode: `production`,
-  devtool: `inline-source-map`
+  mode: `production`
 });
