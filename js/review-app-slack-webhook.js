faa9ffe57a5eecd6e1bb982cca60454bb1d7bf33
Mavis Ou <mmmavis@users.noreply.github.com>
2020-02-26 13:05:51 -0800

    Revert "oops missed this". Because: nope I didn't miss anything.
    This reverts commit 0c994650b0e83022441f70d1b5a5f72ced6e13ca.

--- js/review-app-slack-webhook.js
+++ js/review-app-slack-webhook.js
@@ -43,8 +43,8 @@ const request = (url, options, postData = ``) => {
   });
 };
 
-const postToSlack = payload => {
-  let data = JSON.stringify(payload);
+const postToSlack = () => {
+  let data = JSON.stringify(slack_payload);
 
   request(
     slack_webhook,
