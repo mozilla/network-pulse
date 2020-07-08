78bbd9c7933cc4dd574f62a4d1deeee7e5290613
Mavis Ou <mmmavis@users.noreply.github.com>
2020-07-08 09:44:25 -0700

    code improvement

--- pages/add/form/tag-delimiters.js
+++ pages/add/form/tag-delimiters.js
@@ -1,10 +1,5 @@
-// keycodes for tab,enter,comma, used with
-// the react-tags-autocomplete component
-// The delimiters option is now an array of KeyboardEvent.key values and
-// not KeyboardEvent.keyCode codes,
-// e.g. [13, 9] should now be written as ['Enter', 'Tab']. See https://keycode.info/ for more information.
-
-// It seems like react-tags-autocomplete no longer accepts Comma as delimiter
-// GitHub ticket filed: https://github.com/i-like-robots/react-tags/issues/213
-const DELIMITERS = [`Tab`, `Enter`];
+// keycodes for tab,enter,comma, used with the react-tags-autocomplete component
+// Delimiters map to `KeyboardEvent.key` values,
+// see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Value
+const DELIMITERS = [`Tab`, `Enter`, `,`];
 export { DELIMITERS };
