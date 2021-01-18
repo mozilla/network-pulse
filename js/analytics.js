6a1ccb288eef603050f513ff6b304666213c024b
Mavis Ou <mmmavis@users.noreply.github.com>
2021-01-18 11:22:46 -0800

    improvements

--- js/analytics.js
+++ js/analytics.js
@@ -2,59 +2,54 @@ import ReactGA from "react-ga";
 
 let Analytics = {};
 
-if (typeof window === `undefined`) {
-  // script is being run server-side
+function checkDoNotTrack() {
+  // Treat server-side prerender as "do not track". If JS is enabled, the
+  // React app will load client-side and add in GA later if permitted.
+  if (typeof window === `undefined`) return true;
 
-  // A no-operation object with the same API surface as ReactGA
-  Analytics.ReactGA = {
-    initialize: () => {},
-    event: () => {},
-  };
-  Analytics.initialize = () => {};
-} else {
-  // script is being run client-side
+  // Check user's DNT setting is script is being run client-side
+  let _dntStatus = navigator.doNotTrack || navigator.msDoNotTrack,
+    fxMatch = navigator.userAgent.match(/Firefox\/(\d+)/),
+    ie10Match = navigator.userAgent.match(/MSIE 10/i),
+    w8Match = navigator.appVersion.match(/Windows NT 6.2/);
 
-  Analytics.ReactGA = ReactGA;
-  Analytics.initialize = () => {
-    const checkDoNotTrack = () => {
-      let _dntStatus = navigator.doNotTrack || navigator.msDoNotTrack,
-        fxMatch = navigator.userAgent.match(/Firefox\/(\d+)/),
-        ie10Match = navigator.userAgent.match(/MSIE 10/i),
-        w8Match = navigator.appVersion.match(/Windows NT 6.2/);
+  if (fxMatch && Number(fxMatch[1]) < 32) {
+    _dntStatus = `Unspecified`;
+  } else if (ie10Match && w8Match) {
+    _dntStatus = `Unspecified`;
+  } else {
+    _dntStatus =
+      {
+        0: `Disabled`,
+        1: `Enabled`,
+      }[_dntStatus] || `Unspecified`;
+  }
 
-      if (fxMatch && Number(fxMatch[1]) < 32) {
-        _dntStatus = `Unspecified`;
-      } else if (ie10Match && w8Match) {
-        _dntStatus = `Unspecified`;
-      } else {
-        _dntStatus =
-          {
-            0: `Disabled`,
-            1: `Enabled`,
-          }[_dntStatus] || `Unspecified`;
-      }
+  return _dntStatus === `Enabled`;
+}
 
-      return _dntStatus === `Enabled`;
-    };
+// Are we allowed to track?
+if (checkDoNotTrack() === false) {
+  // build our analytics export
+  Analytics = {
+    ReactGA,
+    initialize: () => {
+      const getIdentifier = (type = "ga") => {
+        const meta = document.querySelector(`meta[name="${type}-identifier"]`);
 
-    const getIdentifier = (type = "ga") => {
-      const meta = document.querySelector(`meta[name="${type}-identifier"]`);
+        if (!meta) return;
 
-      if (!meta) return;
+        let identifier = meta.getAttribute(`content`);
 
-      let identifier = meta.getAttribute(`content`);
+        if (!identifier) {
+          console.warn(
+            `No ${type.toUpperCase()} identifier found: skipping bootstrap step`
+          );
+        }
 
-      if (!identifier) {
-        console.warn(
-          `No ${type.toUpperCase()} identifier found: skipping bootstrap step`
-        );
-      }
+        return identifier;
+      };
 
-      return identifier;
-    };
-
-    // if DO NOT TRACK is not enabled
-    if (!checkDoNotTrack()) {
       const GA_ID = getIdentifier(`ga`);
       const GTM_ID = getIdentifier(`gtm`);
 
@@ -74,7 +69,20 @@ if (typeof window === `undefined`) {
           f.parentNode.insertBefore(j, f);
         })(window, document, "script", "dataLayer", GTM_ID);
       }
-    }
+    },
+  };
+}
+
+// If we're not allowed to track, return a "no-op" API object.
+else {
+  function noop() {}
+
+  Analytics = {
+    ReactGA: {
+      initialize: noop,
+      event: noop,
+    },
+    initialize: noop,
   };
 }
 
